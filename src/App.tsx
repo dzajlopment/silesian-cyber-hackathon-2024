0cf36c2df1496468faf4694c96290dc03b950150
Bartosz Bieniek <itsbk20@gmail.com>
2024-04-04 23:17:01 +0200

    Reformat files with Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
 export const App = () => {
-  return <h1>Hello, React</h1>
-}
+  return <h1>Hello, React</h1>;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {App} from './App.tsx';
+import './index.css';
 
-const root = document.getElementById('root')!
+const root = document.getElementById('root')!;
 
-ReactDOM
-  .createRoot(root)
-  .render(
-    <React.StrictMode>
-      <App/>
-    </React.StrictMode>,
-  )
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
