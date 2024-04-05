3012187b83c0b70ad39ab03b15596c07e92ac556
LordPrinz <plazinski2902@gmail.com>
2024-04-05 14:17:38 +0200

    Add points

--- src/_features/map/Points.tsx
+++ src/_features/map/Points.tsx
@@ -1,3 +1,4 @@
+import {InfoVariants} from './Map';
 import Point from './Point';
 
 // Temporary array of points with their properties
@@ -9,6 +10,20 @@ const POINTS = [
     variant: 'water', // Variant type for styling the point
     isDangerous: false, // Boolean flag indicating if the point is dangerous
   },
+  {
+    id: 'report_2',
+    points: [51.315, -0.1], // Latitude and longitude of the point
+    text: '10', // Text content for the point
+    variant: 'gas', // Variant type for styling the point
+    isDangerous: false, // Boolean flag indicating if the point is dangerous
+  },
+  {
+    id: 'report_3',
+    points: [51.205, 0.09], // Latitude and longitude of the point
+    text: '80', // Text content for the point
+    variant: 'electricity', // Variant type for styling the point
+    isDangerous: false, // Boolean flag indicating if the point is dangerous
+  },
 ] as const;
 
 const Points = () => {
@@ -18,7 +33,7 @@ const Points = () => {
         <Point
           position={p.points as [number, number]} // Setting the position of the point
           text={p.text} // Setting the text content of the point
-          variant={p.variant} // Setting the variant type of the point
+          variant={p.variant as InfoVariants} // Setting the variant type of the point
           key={p.id} // Setting the unique key for each point
           dangerous={p.isDangerous} // Setting the danger flag for the point
         />
--- src/_features/map/device/modal-info.tsx
+++ src/_features/map/device/modal-info.tsx
@@ -1,4 +1,10 @@
+import {useParams} from 'react-router-dom';
+
 const ModalInfo = () => {
+  const {id} = useParams();
+
+  console.log(id);
+
   return <div></div>;
 };
 
