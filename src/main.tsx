a9475485c2b361d395144ed0efacfddc0dec8092
Bartosz Bieniek <itsbk20@gmail.com>
2024-04-05 09:54:58 +0200

    Install react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {App} from './App.tsx';
 import './index.css';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Hello</h1>,
+  },
+]);
 
 const root = document.getElementById('root')!;
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
