84f010296f71a6e41aef488f7e6fa47bc9f2b0d9
Bartosz Bieniek <itsbk20@gmail.com>
2024-04-05 12:04:19 +0200

    Create live usage screen

--- src/_features/live-usage/LiveUsageScreen.tsx
+++ src/_features/live-usage/LiveUsageScreen.tsx
@@ -1,3 +1,40 @@
+import {Device} from './_models/Device.ts';
+import {LiveUsageTable} from './live-usage-table/LiveUsageTable.tsx';
+
+const DEVICES_DUMMY_DATA: Device[] = [
+  {
+    id: 'pm-1',
+    type: 'power-meter',
+    location: 'Laboratory Power Station',
+    usageValue: 2,
+    usageUnit: 'kWh',
+    status: 'ok',
+  },
+  {
+    id: 'pm-2',
+    type: 'power-meter',
+    location: 'Main Power Station',
+    usageValue: 11.4,
+    usageUnit: 'kWh',
+    status: 'no-signal',
+  },
+  {
+    id: 'pm-2',
+    type: 'power-meter',
+    location: 'Main Power Station',
+    usageValue: 0,
+    usageUnit: 'kWh',
+    status: 'alert',
+    alertDescription: 'No power',
+  },
+];
+
 export const LiveUsageScreen = () => {
-  return <h1>Live usage</h1>;
+  const devices = DEVICES_DUMMY_DATA;
+
+  return (
+    <main className="px-6">
+      <LiveUsageTable devices={devices} />
+    </main>
+  );
 };
