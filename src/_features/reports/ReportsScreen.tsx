d392c3f590bfc37544dba59c0dc420471122a1e7
Bartosz Bieniek <itsbk20@gmail.com>
2024-04-05 14:08:15 +0200

    Create reports menu screen

--- src/_features/reports/ReportsScreen.tsx
+++ src/_features/reports/ReportsScreen.tsx
@@ -1,11 +1,35 @@
+import {Report} from './_models/Report.ts';
+import {ReportsMenu} from './reports-menu/ReportsMenu.tsx';
+
+const REPORTS_DUMMY_DATA: Report[] = [
+  {
+    id: 're-1',
+    type: 'electricity',
+    createdAt: new Date(),
+  },
+  {
+    id: 're-2',
+    type: 'water',
+    createdAt: new Date(),
+  },
+  {
+    id: 're-3',
+    type: 'water',
+    createdAt: new Date(),
+  },
+  {
+    id: 're-4',
+    type: 'gas',
+    createdAt: new Date(),
+  },
+];
+
 export const ReportsScreen = () => {
+  const reports = REPORTS_DUMMY_DATA;
+
   return (
     <div className="grid grid-cols-[20%,1fr] grow border-t-[1px] border-separator">
-      <nav>
-        <ul>
-          <li>Electricity, 18h ago</li>
-        </ul>
-      </nav>
+      <ReportsMenu reports={reports} />
       <main className="bg-blurry-white">
         <article className="mx-6 p-6 bg-white">
           <header className="flex items-baseline gap-2 mb-4">
